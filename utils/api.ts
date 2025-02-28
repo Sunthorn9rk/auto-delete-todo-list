c66b367e6999bb5f63d36d95f79a2a73bf14cefa
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-28 15:36:45 +0700

    Create api.ts
    Create utils API for get data from API

---
+++ utils/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import {User} from "../src/types";
+
+export const fetchUsers = async (): Promise<User[]> => {
+  try {
+    const response = await axios.get("https://dummyjson.com/users");
+    return response.data.users; // Extract users array
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return [];
+  }
+};
