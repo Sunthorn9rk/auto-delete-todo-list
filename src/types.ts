6670ea1b9d3912ba09f0f44333a63c2e4db79d90
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-28 15:38:00 +0700

    Update types.ts
    Add define type for user

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,16 @@
-//Define a type for the todo item
+//Define type for the todo item
 export type TodoItem = {
   type: "Fruit" | "Vegetable";
   name: string;
 };
+
+// Define type for the user
+export interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: "male" | "female";
+  hair: {color: string};
+  address: {postalCode: string};
+  company: {department: string};
+}
