3748c60141611a38b460518f1fcd41cbb641f2d3
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2025-02-28 15:26:45 +0700

    Create UserTable.tsx
    Create Table for show data

---
+++ src/components/UserTable.tsx
@@ -0,0 +1,31 @@
+"use client";
+import {useEffect, useState} from "react";
+import {fetchUsers} from "../utils/api";
+import {transformUsers} from "../utils/transform";
+
+export default function UserTable() {
+  interface DepartmentData {
+    male: number;
+    female: number;
+    ageRange: string;
+    hair: Record<string, number>;
+    addressUser: Record<string, string>;
+  }
+  const [data, setData] = useState<Record<string, DepartmentData> | null>(null);
+
+  useEffect(() => {
+    fetchUsers().then((users) => {
+      const transformed = transformUsers(users);
+      setData(transformed);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">User Data Grouped by Department</h1>
+      <pre className="bg-gray-200 p-4 mt-4">
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    </div>
+  );
+}
